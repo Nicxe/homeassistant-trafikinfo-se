854996c7fab8884ef4b0b9c0f007b7a31c31dd10
Niklas V <niklas.vilnersson@gmail.com>
2026-01-08 14:06:54 +0100

    fix: Revise release notes language and formatting
    Updated release notes to change language and formatting.

--- release-notes.hbs.js
+++ release-notes.hbs.js
@@ -1,11 +1,10 @@
+module.exports = `
 ## **Trafikinfo SE – v{{version}}**
 
 ![GitHub Downloads (all assets, specific tag)](https://img.shields.io/github/downloads/nicxe/homeassistant-trafikinfo-se/{{version}}/total)
 
 {{#if prerelease}}
-⚠️ Detta är en **beta release** för test och verifiering.
-{{else}}
-✅ Detta är en **stabil release** för Home Assistant.
+⚠️ This is a **beta release** for testing and verification.
 {{/if}}
 
 ---
@@ -15,16 +14,16 @@
 {{#each commits}}
 - {{subject}}
 {{/each}}
-
 {{/each}}
 
 ---
 
-### **⭐ Support `Trafikinfo SE`**
+### **⭐ Support Trafikinfo SE**
 
-If you enjoy using `Trafikinfo SE` and want to support its continued development, consider buying me a coffee or becoming a GitHub Sponsor.
+If you enjoy using Trafikinfo SE and want to support its continued development, consider buying me a coffee or becoming a GitHub Sponsor.
 
 [![Buy Me A Coffee](https://img.shields.io/badge/Buy_Me_a_Coffee-fdd734?&logo=buy-me-a-coffee&logoColor=black&style=for-the-badge)](https://www.buymeacoffee.com/NiklasV)  
 [![GitHub Sponsors](https://img.shields.io/badge/GitHub_Sponsors-30363d?style=for-the-badge&logo=github&logoColor=white)](https://github.com/sponsors/Nicxe)
 
-Your support keeps the project alive and moving forward. Thank you 🙌
+Thank you for your support 🙌
+`;
